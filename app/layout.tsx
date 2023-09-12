97c0a0d8d93d947100a555656c4ac706dec08a3d
bucky <buckyhelmsmash@gmail.com>
2023-09-12 20:03:38 +0700

    initate from template, icon assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
-    icon: "/favicon.ico",
+    icon: "/pokeball.svg",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Pokedex",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Welcome to TypeMaster PokeDex, your ultimate destination for exploring Pokémon by their unique types! TypeMaster PokeDex is a simple yet comprehensive website designed to help you discover and learn about Pokémon based on their elemental attributes.",
   mainNav: [
     {
       title: "Home",
